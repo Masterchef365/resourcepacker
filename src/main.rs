81173264f6b4c0fdd0ddffb5a80bf3540e46698e
Masterchef365 <duncan.freeman1@gmail.com>
2021-06-13 15:29:50 -0700

    Add megatexture compiler

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::{bail, Context, Result};
+use anyhow::{Context, Result, bail, ensure};
 use zip::ZipArchive;
 use std::{fs::File, io::prelude::*};
 
@@ -49,6 +49,10 @@ fn pack(
         todo!("Load atlas")
     };
 
+    let megatexture = compile_megatexture(&mut res_pack_archive, &atlas)?;
+    let mut out_file = File::create(texture_out_dir)?;
+    write_texture_rgb(&mut out_file, &megatexture);
+
     dbg!(atlas);
 
     Ok(())
@@ -57,7 +61,8 @@ fn pack(
 /// Width of image patches
 const TEX_WIDTH: u32 = 16;
 /// Size of image patches in bytes
-const TEX_SIZE: u32 = TEX_WIDTH * TEX_WIDTH * 3 * 4;
+const TEX_CHANNELS: u32 = 3;
+const TEX_SIZE: u32 = TEX_WIDTH * TEX_WIDTH * TEX_CHANNELS;
 
 /// Check png info to see if it is compatible
 fn check_info(info: &png::OutputInfo) -> bool {
@@ -109,10 +114,100 @@ fn create_atlas<R: Read + Seek>(archive: &mut ZipArchive<R>, pack_name: String) 
     })
 }
 
+struct RgbImage {
+    data: Vec<u8>,
+    /// Width in pixels
+    width: u32
+}
+
+impl RgbImage {
+    pub fn new(width: u32, height: u32) -> Self {
+        Self {
+            data: vec![0; (width * height * TEX_CHANNELS) as usize],
+            width,
+        }
+    }
+
+    pub fn blit(&mut self, x: u32, y: u32, other: &RgbImage) {
+        let (my_width, my_height) = self.dimensions();
+        assert!(x < my_width && y < my_height, "Attempt to blit outside image boundaries");
+        // TODO: More asserts!
+        
+        for (row_idx, row) in other.data.chunks_exact(other.row_stride()).enumerate() {
+            let off = self.row_stride() * (row_idx + y as usize) + (x * TEX_CHANNELS) as usize;
+            self.data[off..off+other.row_stride()].copy_from_slice(row);
+        }
+    }
+
+    /// Row width in bytes
+    pub fn row_stride(&self) -> usize {
+        (self.width * TEX_CHANNELS) as _
+    }
+
+    pub fn dimensions(&self) -> (u32, u32) {
+        (self.width, self.data.len() as u32 / (self.width * TEX_CHANNELS))
+    }
+}
+
+fn rgba_to_rgb(data: Vec<u8>) -> Vec<u8> {
+    let mut out = vec![];
+    for chunk in data.chunks_exact(4) {
+        out.push(chunk[0]);
+        out.push(chunk[1]);
+        out.push(chunk[2]);
+    }
+    out
+}
+
+fn read_texture_rgb<R: Read>(reader: &mut R) -> Result<RgbImage> {
+    let decoder = png::Decoder::new(reader);
+    let (info, mut reader) = decoder.read_info()?;
+    let mut data = vec![0; (info.width * info.height * TEX_CHANNELS) as usize];
+    reader.next_frame(&mut data)?;
+
+    let rgb_data = match info.color_type {
+        png::ColorType::RGB => data,
+        png::ColorType::RGBA => rgba_to_rgb(data),
+        other => bail!("Unsupported color type {:?}", other),
+    };
+
+    Ok(RgbImage {
+        data: rgb_data,
+        width: info.width,
+    })
+}
+
+fn write_texture_rgb<W: Write>(writer: &mut W, image: &RgbImage) -> Result<()> {
+    let (width, height) = image.dimensions();
+    let mut encoder = png::Encoder::new(writer, width, height);
+    encoder.set_color(png::ColorType::RGB);
+    encoder.set_depth(png::BitDepth::Eight);
+    let mut writer = encoder.write_header()?;
+    writer.write_image_data(&image.data)?;
+    Ok(())
+}
+
+fn compile_megatexture<R: Read + Seek>(archive: &mut ZipArchive<R>, atlas: &Atlas) -> Result<RgbImage> {
+    // Create megatexture
+    let mut megatexture = RgbImage::new(TEX_WIDTH * atlas.side_length, TEX_WIDTH * atlas.side_length);
+
+    // Blit squares onto the texture
+    for square in &atlas.squares {
+        let mut file = archive.by_name(&square.name).with_context(|| format!("Archive missing {}", &square.name))?;
+        let texture = read_texture_rgb(&mut file).with_context(|| format!("Error reading texture {}", &square.name))?;
+        let (width, height) = texture.dimensions();
+        ensure!(width == TEX_WIDTH && height == TEX_WIDTH, "Textures must be {0}x{0}; {1} is not.", TEX_WIDTH, square.name);
+        megatexture.blit(square.x * TEX_WIDTH, square.y * TEX_WIDTH, &texture);
+    }
+
+    Ok(megatexture)
+}
+
 #[derive(Debug, Clone)]
 struct Atlas {
     pack_name: String,
     side_length: u32,
+    /// Atlas squares. Note: Always in left-right top-bottom order!
     squares: Vec<AtlasSquare>,
 }
 
